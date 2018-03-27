0bb6725f1e8811a70bbbb59b80b3f0750d655c91
guozhiqiang <156627977@qq.com>
2018-03-27 23:44:39 +0800

    scala 新建

---
+++ scala/src/Demo.scala
@@ -0,0 +1,13 @@
+/**
+  * @description
+  * @author guozhiqiang
+  * @created 2018-03-27 22:38.
+  */
+object Demo {
+
+  def main(args: Array[String]): Unit = {
+    println("a");
+    var a = 1 + 1;
+    println(a);
+  }
+}
