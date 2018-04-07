79dc7b32598a65502b727ea2b1a485e82163e7bb
guozhiqiang <156627977@qq.com>
2018-04-07 22:05:00 +0800

    tl juc example

---
+++ juc/src/main/java/com/gzq/juc/lock/TestABCAlternatePrint.java
@@ -0,0 +1,125 @@
+package com.gzq.juc.lock;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author guozhiqiang
+ * @description
+ * @created 2018-04-02 20:56.
+ */
+public class TestABCAlternatePrint {
+
+
+    public static void main(String[] args) {
+        Alternate alternate = new Alternate();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for (int i = 0; i < 5; i++) {
+                    alternate.LoopA(i);
+                }
+
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for (int i = 0; i < 5; i++) {
+                    alternate.LoopB(i);
+                }
+
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for (int i = 0; i < 5; i++) {
+                    alternate.LoopC(i);
+                }
+
+            }
+        }).start();
+
+
+    }
+
+
+    static class Alternate {
+
+        private volatile int number = 1;
+
+        ReentrantLock lock = new ReentrantLock();
+
+        Condition condition1 = lock.newCondition();
+        Condition condition2 = lock.newCondition();
+        Condition condition3 = lock.newCondition();
+
+        public void LoopA(int loops) {
+            lock.lock();
+            try {
+                if (number != 1) {
+                    try {
+                        condition1.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                System.out.println(Thread.currentThread().getName() + " :" + "A " + loops);
+                number = 2;
+                condition2.signal();
+            } finally {
+                lock.unlock();
+            }
+
+        }
+
+
+        public void LoopB(int loops) {
+            lock.lock();
+            try {
+                if (number != 2) {
+                    try {
+                        condition2.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                System.out.println(Thread.currentThread().getName() + " :" + "B " + loops);
+                number = 3;
+                condition3.signal();
+            } finally {
+                lock.unlock();
+            }
+
+        }
+
+        public void LoopC(int loops) {
+            lock.lock();
+            try {
+                if (number != 3) {
+                    try {
+                        condition3.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                System.out.println(Thread.currentThread().getName() + " :" + "C " + loops);
+                number = 1;
+                condition1.signal();
+            } finally {
+                lock.unlock();
+            }
+
+        }
+
+    }
+
+}
---
+++ juc/src/main/java/com/gzq/juc/lock/TestReentrantReadWriteLock.java
@@ -0,0 +1,11 @@
+package com.gzq.juc.lock;
+
+/**
+ * @author guozhiqiang
+ * @description
+ * @created 2018-04-02 21:42.
+ */
+public class TestReentrantReadWriteLock {
+
+
+}
